d7757331e11763dbc0818703a328da0cf8d90844
Dalton Harrold <djharrold1@gmail.com>
2026-01-28 15:56:57 +0000

    playing around with some classes, objects, and builders

--- playground/main.py
+++ playground/main.py
@@ -1,4 +1,30 @@
+class Thing:
+    a: int
+    b: int
+    def __init__(self) -> None:
+        pass
+    
+    def setA(self, a: int) -> None:
+        self.a = a
+        
+    def setB(self, b: int) -> None:
+        self.b = b
+        
+    def printa(self) -> None:
+        print(self.a)
+        
+    def withA(self, a: int) -> Thing:
+        self.setA(a)
+        return self
+    
+    def withB(self, b: int) -> Thing:
+        self.setB(b)
+        return self
+    
+
 def funny(x: int) -> int:
     return x
 
-print(funny("3"))
+my_thing = Thing().withA(3).withB(funny(4))
+my_thing.printa()
+print(my_thing.b)
